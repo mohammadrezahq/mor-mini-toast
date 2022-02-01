f43b840762581bc3b890eba23d91892ce27b5760
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-02-01 03:55:06 +0330

    Move Get Icon Function to New File

--- inc/app.js
+++ inc/app.js
@@ -1,5 +1,6 @@
 const Style = require('./style.js')
 const defaultOptions = require('./defaultOptions.js')
+const icon = require('./icon.js')
 
 class app {
 
@@ -33,7 +34,7 @@ class app {
         if (this.options.canClose === true) {
             const closeElement = document.createElement('span');
             closeElement.classList.add('close-toast-button');
-            closeElement.innerHTML += this.getIcon('close');
+            closeElement.innerHTML += icon.getIcon('close', this.options.advanced.closeIconColor);
             let event = this;
             closeElement.addEventListener('click', function() { closeToastWithButton(event.e, event.exitStyle) })
             this.e.appendChild(closeElement);
@@ -50,35 +51,9 @@ class app {
     setIcon() {
 
         if (this.type !== 'simple') {
-            this.e.innerHTML += "<span style='margin:0 5px; display:inline-block; vertical-align: middle;'>" + this.getIcon(this.type) + "</span>";
+            this.e.innerHTML += "<span style='margin:0 5px; display:inline-block; vertical-align: middle;'>" + icon.getIcon(this.type, this.options.iconColor) + "</span>";
         }
 
-    }
-
-    getIcon(type) {
-        if (type === 'success') {
-            return `
-<svg color="` + this.options.iconColor + `"xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 36 36"><path class="clr-i-outline clr-i-outline-path-1" d="M18 2a16 16 0 1 0 16 16A16 16 0 0 0 18 2zm0 30a14 14 0 1 1 14-14a14 14 0 0 1-14 14z" fill="currentColor"/><path class="clr-i-outline clr-i-outline-path-2" d="M28 12.1a1 1 0 0 0-1.41 0l-11.1 11.05l-6-6A1 1 0 0 0 8 18.53L15.49 26L28 13.52a1 1 0 0 0 0-1.42z" fill="currentColor"/></svg>
-`
-        }
-
-        if (type === 'alert') {
-            return `
-<svg color="` + this.options.iconColor + `"xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"/><path d="M12 7v6m0 3.5v.5" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></g></svg>
-`
-        }
-
-        if (type === 'error') {
-            return `
-<svg color="` + this.options.iconColor + `"xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><g fill="currentColor"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.6 1c1.6.1 3.1.9 4.2 2c1.3 1.4 2 3.1 2 5.1c0 1.6-.6 3.1-1.6 4.4c-1 1.2-2.4 2.1-4 2.4c-1.6.3-3.2.1-4.6-.7c-1.4-.8-2.5-2-3.1-3.5C.9 9.2.8 7.5 1.3 6c.5-1.6 1.4-2.9 2.8-3.8C5.4 1.3 7 .9 8.6 1zm.5 12.9c1.3-.3 2.5-1 3.4-2.1c.8-1.1 1.3-2.4 1.2-3.8c0-1.6-.6-3.2-1.7-4.3c-1-1-2.2-1.6-3.6-1.7c-1.3-.1-2.7.2-3.8 1c-1.1.8-1.9 1.9-2.3 3.3c-.4 1.3-.4 2.7.2 4c.6 1.3 1.5 2.3 2.7 3c1.2.7 2.6.9 3.9.6zM7.9 7.5L10.3 5l.7.7l-2.4 2.5l2.4 2.5l-.7.7l-2.4-2.5l-2.4 2.5l-.7-.7l2.4-2.5l-2.4-2.5l.7-.7l2.4 2.5z"/></g></svg>
-`
-        }
-
-        if (type === 'close') {
-            return `
-<svg color="` + this.options.advanced.closeIconColor + `" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42z"/></svg>
-            `
-        }
     }
 
     styleElement() {
